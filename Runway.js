693b4965915d2679e8da40b16a7d652ecfb9e336
rekhan <rekhan@expediagroup.com>
2023-02-10 19:58:36 +0000

    finished all tests

--- Runway.js
+++ Runway.js
@@ -1,18 +1,27 @@
 class Runway {
-    static MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS = 100;
-    static planes = [];
+  static MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS = 100;
+  static planes = [];
 
-    name;
+  name;
 
-    constructor(name) {
-        this.name = name
+  constructor(name) {
+    this.name = name;
+  }
+
+  add(plane) {
+    if (Runway.planes.length > Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
+      throw new Error("runways at full capacity!");
     }
-    
-    add(plane) {
+    Runway.planes.push(plane);
+  }
 
-        if (Runway.planes.length>Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
-            throw new Error ("runways at full capacity!")
-        }
-        Runway.planes.push(plane)
+  remove(plane) {
+    if (Runway.planes.length === 0) {
+      throw new Error("no planes in runways");
     }
+    const indexOfPlane = Runway.planes.indexOf(plane);
+    Runway.planes.splice(indexOfPlane, 1);
+  }
 }
+
+module.exports = Runway;
